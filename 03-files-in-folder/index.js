3fa2f6145a8e7e0255e8b5b084e43e5b676516e4
unknown <kodywinq1@gmail.com>
2022-11-02 15:32:01 +0300

    feat: solve 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+  fs.readdir("./03-files-in-folder/secret-folder", { withFileTypes: true }, (err, files) => {
+    if(err)
+        console.log(err);
+    else{
+        files.forEach(file => {
+            if(file.isFile()){
+                let name;
+                let extension;
+                let size;
+                let filePath = path.join(__dirname, "secret-folder", file.name);
+                let dataFile = path.parse(filePath);
+                fs.stat(filePath, (error, stats) => {
+                    name = dataFile.name;
+                    extension = dataFile.ext;
+                    size = stats.size;
+                    console.log(`${name} - ${extension} - ${size}b`);
+                });
+            } 
+        });
+    }
+  });
