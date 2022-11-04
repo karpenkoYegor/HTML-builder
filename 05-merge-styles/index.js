93d14761371b135e408f9d630c74f90b2321035f
unknown <kodywinq1@gmail.com>
2022-11-04 18:08:10 +0300

    feat: solve 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.writeFile(path.join(__dirname, "project-dist", "bundle.css"), "", (err) =>{
+    if(err){
+        throw err;
+    }
+});
+
+fs.readdir(path.join(__dirname,"styles"), { withFileTypes: true }, (err, files) => {
+    if(err)
+        console.log(err);
+    else{
+        files.forEach(file => {
+            if(file.isFile())
+            {
+                let filePath = path.join(__dirname, "styles", file.name);
+                let dataFile = path.parse(filePath);
+                
+                extension = dataFile.ext;
+                if(extension === ".css"){
+                    const stream = new fs.ReadStream(filePath, { encoding: "utf-8" });
+
+                    stream.on("readable", function(){
+                        let data = stream.read();
+                        fs.appendFile(path.join(__dirname, "project-dist", "bundle.css"), `${data}\n`, (err) =>{
+                            if(err){
+                                throw err;
+                            }
+                        });
+                        console.log(path.join(__dirname, "project-dist", "bundle.css"));
+                        stream.destroy();
+                    });
+                    
+                }
+            }
+        });
+    }
+});
